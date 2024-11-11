287a157ebc04d12c0f699ccded84f04491e94e3d
Prosperg <promasdev@gmail.com>
2024-11-11 17:36:34 +0100

    Initial

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Article;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Article::truncate();
+        \App\Models\Article::unguard();
+
+        $faker = \Faker\Factory::create();
+
+        \App\Models\User::all()->each(function ($user) use ($faker) {
+            foreach (range(1, 5) as $i) {
+                \App\Models\Article::create([
+                    'user_id' => $user->id,
+                    'title'   => $faker->sentence,
+                    'content' => $faker->paragraphs(3, true),
+                ]);
+            }
+        });
+    }
+}
